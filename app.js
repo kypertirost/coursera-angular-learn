a71d392b58a849250f8bd7d1714b5938fe377031
Miao Liu <liumiao1018@me.com>
2021-09-11 19:38:12 -0400

    organize branch

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-(function(){
-'use strict';
-  angular.module('DIApp', [])
-  .controller("DIController", DIController);
-  DIController.$inject = ['$scope', '$filter'];//for minification
-  function DIController($scope, $filter){
-    $scope.name = "Kyper";
-    $scope.upper = () => {
-        var upCase = $filter('uppercase');
-        $scope.name = upCase($scope.name);
-    };
+(function() {
+  'use strict';
+  angular.module('LunchCheck',[]).controller("LunchCheckController", LunchCheckController);
+  LunchCheckController.$inject =['$scope'];
+  function LunchCheckController($scope){
+    $scope.textColor = {"color":"black"};
+    $scope.listOfFood="";
+    $scope.result='Please enter your food';
+    $scope.displayTooMuch = function() {
+      let num = calculateLength($scope.listOfFood);
+      let evalFromFood = evaluateString(num);
+      console.log(num);
+      $scope.result = evalFromFood;
+      if(num <=0) {
+        $scope.textColor = {"color":"red"};
+      } else {
+        $scope.textColor = {"color":"green"};
+      }
+    }
+  }
+  function calculateLength(string) {
+    if(string == "") {
+      return 0;
+    }
+    let foodArr = string.replace(/\s+/g, "").split(",");
+    let n = 0;
+    foodArr.forEach((item) => {
+      if(item != "") {
+        n ++;
+      }
+    });
+    return n;
+  }
+  function evaluateString(num) {
+    // if(string == "") {
+    //   return "Please enter data first";
+    // }
+    // let foodArr = string.replace(/\s+/g, "").split(",");
+    // let n = 0;
+    // foodArr.forEach((item) => {
+    //   if(item != "") {
+    //     n ++;
+    //   }
+    // });
+    // if(n > 3) {
+    //   return "Too much!";
+    // } else {
+    //   return "Enjoy!";
+    // }
+    if(num <=0 ){
+      return "Please enter data first";
+    } else if (num > 3){
+      return "Too much!";
+    } else {
+      return "Enjoy!";
+    }
   }
 })();
-// !function(){"use strict";function e(e,n){e.name="Kyper",e.upper=(()=>{var r=n("uppercase");e.name=r(e.name)})}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
