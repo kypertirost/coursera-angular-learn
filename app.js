ce1ed94de2cbabe3f23fee5e6a2effdd2a5de765
Miao Liu <liumiao1018@me.com>
2021-09-11 17:00:32 -0400

    minification injection

--- app.js
+++ app.js
@@ -2,8 +2,13 @@
 'use strict';
   angular.module('DIApp', [])
   .controller("DIController", DIController);
-
-  function DIController($scope){
+  DIController.$inject = ['$scope', '$filter'];//for minification
+  function DIController($scope, $filter){
     $scope.name = "Kyper";
+    $scope.upper = () => {
+        var upCase = $filter('uppercase');
+        $scope.name = upCase($scope.name);
+    };
   }
 })();
+// !function(){"use strict";function e(e,n){e.name="Kyper",e.upper=(()=>{var r=n("uppercase");e.name=r(e.name)})}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
