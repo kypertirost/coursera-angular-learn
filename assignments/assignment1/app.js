23303ce28ea858824487eaded7c57bac0d27c957
Miao Liu <liumiao1018@me.com>
2021-09-11 19:32:49 -0400

    complete all bonus

--- assignments/assignment1/app.js
+++ assignments/assignment1/app.js
@@ -3,16 +3,24 @@
   angular.module('LunchCheck',[]).controller("LunchCheckController", LunchCheckController);
   LunchCheckController.$inject =['$scope'];
   function LunchCheckController($scope){
+    $scope.textColor = {"color":"black"};
     $scope.listOfFood="";
     $scope.result='Please enter your food';
     $scope.displayTooMuch = function() {
-      var evalFromFood = tooMuchFood($scope.listOfFood);
+      let num = calculateLength($scope.listOfFood);
+      let evalFromFood = evaluateString(num);
+      console.log(num);
       $scope.result = evalFromFood;
+      if(num <=0) {
+        $scope.textColor = {"color":"red"};
+      } else {
+        $scope.textColor = {"color":"green"};
+      }
     }
-  };
-  function tooMuchFood(string) {
+  }
+  function calculateLength(string) {
     if(string == "") {
-      return "Please enter data first";
+      return 0;
     }
     let foodArr = string.replace(/\s+/g, "").split(",");
     let n = 0;
@@ -21,11 +29,30 @@
         n ++;
       }
     });
-    if(n > 3) {
+    return n;
+  }
+  function evaluateString(num) {
+    // if(string == "") {
+    //   return "Please enter data first";
+    // }
+    // let foodArr = string.replace(/\s+/g, "").split(",");
+    // let n = 0;
+    // foodArr.forEach((item) => {
+    //   if(item != "") {
+    //     n ++;
+    //   }
+    // });
+    // if(n > 3) {
+    //   return "Too much!";
+    // } else {
+    //   return "Enjoy!";
+    // }
+    if(num <=0 ){
+      return "Please enter data first";
+    } else if (num > 3){
       return "Too much!";
     } else {
-      return "Enjoiy!";
+      return "Enjoy!";
     }
-
   }
 })();
