d98cd7c8ff46854f40a9f8d2767c8bbbf28ef9f3
Miao Liu <liumiao1018@me.com>
2021-09-11 18:43:40 -0400

    complete basic functionality

--- assignments/assignment1/app.js
+++ assignments/assignment1/app.js
@@ -3,15 +3,29 @@
   angular.module('LunchCheck',[]).controller("LunchCheckController", LunchCheckController);
   LunchCheckController.$inject =['$scope'];
   function LunchCheckController($scope){
-    $scope.food='';
+    $scope.listOfFood="";
     $scope.result='Please enter your food';
     $scope.displayTooMuch = function() {
-      var evalFromFood = tooMuchFood($scope.food);
+      var evalFromFood = tooMuchFood($scope.listOfFood);
       $scope.result = evalFromFood;
     }
   };
-
   function tooMuchFood(string) {
-    console.log(string);
+    if(string == "") {
+      return "Please enter data first";
+    }
+    let foodArr = string.replace(/\s+/g, "").split(",");
+    let n = 0;
+    foodArr.forEach((item) => {
+      if(item != "") {
+        n ++;
+      }
+    });
+    if(n > 3) {
+      return "Too much!";
+    } else {
+      return "Enjoiy!";
+    }
+
   }
 })();
