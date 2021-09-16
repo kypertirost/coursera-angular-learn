91999ff514cf1f5bde14fae2053e9d6faa11dab4
Miao Liu <liumiao1018@me.com>
2021-09-16 14:00:47 -0400

    update file

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 (function(){
 'use strict';
   angular.module('DIApp', [])
-  .controller("DIController", DIController);
-  DIController.$inject = ['$scope', '$filter'];//for minification
-  function DIController($scope, $filter){
+  .controller("DIController", DIController)
+  .filter('loves', LovesFilter)
+  .filter('truth', TruthFilter);
+  DIController.$inject = ['$scope', '$filter', 'lovesFilter'];//for minification
+  function DIController($scope, $filter, lovesFilter){
     $scope.name = "Kyper";
     $scope.upper = () => {
-        var upCase = $filter('uppercase');
-        $scope.name = upCase($scope.name);
+        $scope.name = lovesFilter($scope.name);
     };
+  }
+
+  function LovesFilter(){
+    return function(input) {
+      input = input || "";
+      input = input.replace("likes", "love");
+      return input;
+    }
+  }
+
+  function TruthFilter(){
+    return function(input, target, replace) {
+      input = input || "";
+      input = input.replace(target, replace);
+      return input;
+    }
   }
 })();
 // !function(){"use strict";function e(e,n){e.name="Kyper",e.upper=(()=>{var r=n("uppercase");e.name=r(e.name)})}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
